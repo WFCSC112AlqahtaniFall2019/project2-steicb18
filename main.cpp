3ce9e7bf8715b635af2b3d5da5eaff7d198c7177
alqahtanisarra <sarra.alqahtani@gmail.com>
2019-09-12 09:30:17 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function declarations
+void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right);
+void mergeSortedLists(vector<int>& a, vector<int>& tmp, int left, int middle, int right);
+
+int main() {
+
+    // get input: first is random seed, second is vector length
+    int seed, length;
+    cin >> seed >> length;
+    srand(seed);
+
+    vector<int> v(length);  // vector to be sorted
+    vector<int> t(length);  // temporary workspace
+    // unit test for merge
+    /* your code here */
+    // initialize and print input
+    for(int i = 0; i < v.size(); i++) {
+        v.at(i) = rand() % 100;
+        cout << v.at(i) << '\t';
+    }
+    cout << endl;
+
+    // sort v
+    /* your code here */
+
+    // print output
+    for(int i = 0; i < v.size(); i++) {
+        cout << v.at(i) << '\t';
+    }
+    cout << endl;
+
+    // check output, make sure it's sorted
+    for(int i = 1; i < v.size(); i++) {
+        assert(v.at(i-1) <= v.at(i));
+    }
+
+    return 0;
+}
+
+/* your code here */
